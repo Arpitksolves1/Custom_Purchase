9e02fc0e5ad3089bc3f0d48562c535bff809bd1c
Arpitksolves1 <113586089+Arpitksolves1@users.noreply.github.com>
2022-09-29 23:28:08 +0530

    Second commit

--- models/custom_purchase_order.py
+++ models/custom_purchase_order.py
@@ -3,29 +3,27 @@
 from odoo import api, fields, models
 
 
-class CustomPurchaseOrder(models.Model):
+class CustomPurchaseOrderLine(models.Model):
     _inherit = "purchase.order"
 
-    custom_product_line = fields.One2many("custom.product.order", "custom_product_id", string="Product Name")
-    # custom_product = fields.Char(string="Name")
-    # custom_product_id = fields.Many2one('purchase.order', string="Name")
+    custom_purchase_line = fields.One2many("custom.product.order", "custom_product_id", string="Product Name")
 
 
 class ProductOrderDetails(models.Model):
     _name = "custom.product.order"
     _description = "Product Order Details"
 
-    quantity = fields.Integer(string='Quantity')
+    def _compute_total_price(self):
+        for rec in self:
+            rec.product_total = rec.quantity * rec.product_price
+
+    quantity = fields.Integer(string='Quantity', default=1)
     product_price = fields.Float(string="Price")
     product_total = fields.Float(string="Total", compute="_compute_total_price")
     custom_product_id = fields.Many2one("purchase.order", string="Product")
-    choose_product_id = fields.Many2one("product.product", string="Product")
+    product_id = fields.Many2one("product.product", string="Product")
 
-    @api.onchange("choose_product_id")
-    def onchange_choose_product_id(self):
-        for i in self:
-            i.product_price = i.choose_product_id.list_price
-
-    def _compute_total_price(self):
-        for i in self:
-            i.product_total = i.quantity * i.product_price
+    @api.onchange("product_id")
+    def onchange_product_id(self):
+        for rec in self:
+            rec.product_price = rec.product_id.list_price
